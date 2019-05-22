06a7425c2d40f311c1cd74e9af9388ce0e19f7c2
Enrique Mendez <eqm@mit.edu>
2019-05-22 10:21:53 -0400

    Updated comments on main file.

--- M4tempController.ino
+++ M4tempController.ino
@@ -1,23 +1,32 @@
 /*  M4tempController.ino
  *  ===============================
- *  
+ *	 A temperature controller built on the Feather M4 Express board.
+ *   External interfacing is done mostly using A0-A5 pins, their usage
+ *   is in this file.
  */
 
 
 //low level software
+/*
+ * Edit these libraries if you have different pin outs!
+ *
+ */ 
 #include "lcd.h"    //includes lcd class for screen communication
 #include "input.h"  //includes keypad inputs
 #include "menu.h"
 
 //utilities
+/*
+ * Header files for graphing. bitArray is a custom boolean array class.
+ */
 #include "bitArray.h"
 #include "graph.h"
 
 //applications
-#include "temperatureController.h"
-#include <RTClib.h>
-#include <math.h>
-#include "MemoryFree.h"
+#include "temperatureController.h"	//holds the object classes that do the controlling
+//#include <RTClib.h>             	//a class for interfacing with external/internal real time clocks
+#include <math.h>                 	
+//#include "MemoryFree.h"         	//a class for checking ram.
 
 #include "ramSettings.h"	//header file to wrap ramSettings menu.
 #include "eomSettings.h"	//need to put into it's own class eventually...
@@ -27,15 +36,21 @@
 	//global variables
 		#define PROGS 6       	//number of programs in the settings menu
 		#define DATAPOINTS 500	//the number of datpoints we hold for the graph.
-		#define SWITCHPIN  A5
+		#define SWITCHPIN  A5 	//pin used for switching between 
 
 	//vars for holding data to plot
+		//for graph of temperatures and drive values
 		float EOMtemps[DATAPOINTS] = {0};
-		float RAMvoltages[DATAPOINTS] = {0};
-		float FiberErrors[DATAPOINTS] = {0};
-		float times[DATAPOINTS] = {0};
-		float RAMtimes[DATAPOINTS] = {0};
 		float outputs[DATAPOINTS] = {0};
+		float times[DATAPOINTS] = {0};
+
+		//for graph of RAM values and times
+		float RAMvoltages[DATAPOINTS] = {0};
+		float RAMtimes[DATAPOINTS] = {0};
+
+		//for graph of fiber errors (not yet used)
+		float FiberErrors[DATAPOINTS] = {0};
+		
 
 	//temperature controllers and time keeper objects
 		RTC_Millis rtc;
@@ -50,11 +65,13 @@
 	//helper functions for cleaner code
 		void printTime();
 		void printHeader();
+		#ifdef MEMORY_FREE_H
 		void printFreeRam(){
 			lcd.println("Free RAM is: ");
 			lcd.println(freeMemory());
 			wait();
 		}
+		#endif
 
 	//functions to be called in the settings menu.
 		void printPinOuts(){
@@ -125,20 +142,21 @@ void setup() {
 	
 	/******* Menu Initializations **********/
 
-	settings.loop = false;                        	//define settings to quit once we call a function
-	                                              	//this is so we return automatically to the graph
+	settings.loop = false;	//define settings to quit once we call a function
+	                      	//this is so we return automatically to the graph
+
 	settings.defineTitle("---menu---");           	//title
 	settings.defineMenuItems(settingsItems,PROGS);	//define items
 	settings.defineMenuProgs(settingsProgs,PROGS);	// "        "
 
 	
 	eomSettings.loop = false;	//same as for settings
-	eomSettings.defineTitle("-Temperature Lock Sett's-");
+	eomSettings.defineTitle("-Temp. Lock Sett's-");
 	eomSettings.defineMenuItems(eomMenu::settingsItems,EOMPROGS);
 	eomSettings.defineMenuProgs(eomMenu::settingsProgs,EOMPROGS);
 
 	ramSettings.loop = false;	//same as for settings
-	ramSettings.defineTitle("-Temperature Lock Sett's-");
+	ramSettings.defineTitle("-RAM Lock Settings-");
 	ramSettings.defineMenuItems(ramMenu::settingsItems,RAMPROGS);
 	ramSettings.defineMenuProgs(ramMenu::settingsProgs,RAMPROGS);
 
@@ -161,9 +179,7 @@ void setup() {
 	                                	//feedback mechanisms are the same! they don't interfere.
 	pinMode(SWITCHPIN, INPUT);
 
-	/******* Load SD Card and print Info! ************/
-
-
+	/******* Load SD Card and print connection nfo! ************/
 	if(SD.begin(SD_CS)){
 		lcd.println("SD card connected!");
 	} else {
@@ -185,19 +201,15 @@ void setup() {
 	activateTempFeedback();	//lock temperature just as a default.
 }
 
-
-DateTime oldPrintTime;
-
 void loop() {
 	cls();	//clear the screen
 	printHeader();
-	printMode(true);
+	printMode(true);	//print which lock we have.
 	
 	//define graph objects, and their plotting area
 		graph plt(0,3*CHARH,lcd.width(),2*CHARH + lcd.height()/2);
 		graph tecPlt(0,2*CHARH + lcd.height()/2, lcd.width()/2, lcd.height());
 		graph ramPlt(lcd.width()/2,2*CHARH + lcd.height()/2, lcd.width(), lcd.height());
-		
 		
 	//temperature plot settings 
 		plt.setBoundary(0);
@@ -220,47 +232,53 @@ void loop() {
 		ramPlt.makeAxes();
 
 
-
 	//declare variables for looping application
-		int i = 0;
-		int j = 0;
+		int i = 0;	//index for temperature/output plotting
+		int j = 0;	//index for RAM plotting
 		unsigned long lastPlotTime = millis();
-		unsigned long oldtime = millis();
-		float avgTemp = NAN;
-		float ramVoltage = NAN;
-		unsigned time;
+		float avgTemp = NAN;   	//temperature measurements
+		float ramVoltage = NAN;	//ram drive measurements
 
-		
-
+	//run time loop.
 	while(true){
-		printMode(false);
+		printMode(false);	//only update label when lock mode has changed.
+
+		/*
+		 *  Switch between appropriate locking modes
+		 *  and call the associated lock function.
+		 */
+
 		//either lock the eom or ram depending on what we want.
 		if( eom.lockbox.locked && digitalRead(SWITCHPIN)){
-			avgTemp = eom.lock();	//eom.lock() measures for a long time then returns the average
+			avgTemp = eom.lock();	//eom.lock() measures for a eom.feedbackTime then returns the average
 			                     	//temperature	 
 		} else if (ram.lockbox.locked && digitalRead(SWITCHPIN)) {
 			ramVoltage = ram.lock();
 			avgTemp = eom.thermistor.getAverageTemperature(eom.averageNumber,3000); //average for three seconds.
 
-			//switch to temperature locking if we've strayed too far. clearly ram lock isn't fedback
+			//switch to temperature locking if we've strayed too far. clearly ram lock isn't fedback properly
 			if(abs(avgTemp - eom.lockbox.setpoint) > 1){
 				activateTempFeedback();
 			}
 		} else if (!digitalRead(SWITCHPIN)){
+			//just log temperature if something else is controlling our eom temperature.
 			avgTemp = eom.thermistor.getAverageTemperature(eom.averageNumber,3000);
 		}
 
 
 
-		//only log temperature if we measured it.
-			if(!isnan(avgTemp)){
+		//Log temperature to SD card and update graph data.
+			if(!isnan(avgTemp)){	//only log temperature if we measured it.
+
 				EOMtemps[i] = avgTemp;
 				times[i] = millis()/1000.;	//this will wrap around in 50? days?
+
 				if(eom.lockbox.locked && digitalRead(SWITCHPIN)){
 					outputs[i] = eom.lockbox.output;
 				} else if (ram.lockbox.locked && digitalRead(SWITCHPIN)){
 					outputs[i] = ram.lockbox.output;
 				}
+
 				i = (i + 1) % DATAPOINTS;
 
 				//log data to sd card
@@ -284,7 +302,8 @@ void loop() {
 				avgTemp = NAN;
 			}
 
-		//only log RAM if we measured it.
+		//only log RAM if we measured it
+		//update RAM graph and SD card values.
 			if(!isnan(ramVoltage)){
 				RAMvoltages[j] = ramVoltage;
 				RAMtimes[j] = millis()/1000.;	//this will wrap around in 50? days?
@@ -313,26 +332,36 @@ void loop() {
 
 		//update plot every second only.
 			if(millis() - lastPlotTime > 1000){
+				//remember when we plotted.
 				lastPlotTime = millis();
+
+				//plot the data to the first trace for each of the graphs.
 				tecPlt.plotData(0, times, outputs, DATAPOINTS);
 				ramPlt.plotData(0, RAMtimes, RAMvoltages, DATAPOINTS);
 				plt.plotData(0, times, EOMtemps, DATAPOINTS);
+
+				//don't forget to draw the graphs!
 				plt.drawGraph();
 				tecPlt.drawGraph();
 				ramPlt.drawGraph();
 			}
-			
+		
+
+
 		//access settings menu if key is pressed
 			char key = keypad.getKey();
 			if(key){
 				if(key == '*'){
+					//kill loop() and then call loop()
+					//for resizing the graphs if we change the screen rotation.
 					return;
 				}
+
 				lcd.setFont();
-				settings.ui();
-				oldtime = millis(); // don't integrate for the huge time we were in menu.
+				settings.ui();	//menu ui call.
+				
+				//reset display
 				cls();
-			
 				printHeader();
 				printMode(true);
 			}
@@ -343,6 +372,10 @@ void loop() {
 
 /******* Helper Functions  ************/
 	void printMode(bool hard){
+		/*
+		 * printLocking mode. If hard==true, always erase and print new label
+		 If hard==false, only print new label if mode has changed.
+		 */
 		static int mode;
 		if(hard){
 			mode = -1;
@@ -382,7 +415,13 @@ void loop() {
 		}
 		lcd.setCursor(x0,y0);
 	}
+
+
 	void printHeader(){
+		/*
+		 * Print pretty info so people know what this box is for.
+		 * And some key press info.
+		 */
 		lcd.setCursor(0,0);
 		GFXfont *oldFont = lcd.getFont();
 		lcd.setFont(&Picopixel);
